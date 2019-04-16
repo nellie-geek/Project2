a6dc8607ee635c09d01e9c96564c88056ef9278d
Natalied84 <n-dreher@hotmail.com>
2019-04-16 10:40:09 -0600

    mmmmmmmeeeeeeeeeeeeeeeeeeeehhhhhhhhh

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var router = require("express").Router();
-var api = require("./api");
-var view = require("./view");
-// var authRoutes = require("./auth");
-
-router.use("/api", api);
-router.use("/", view);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,30 @@
-
+// Requiring necessary npm packages
 var express = require("express");
-var exphbs = require("express-handlebars");
-var app = express();
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
-var routes = require("./routes");
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 8080;
 var db = require("./models");
-var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+// Creating express app and configuring middleware needed for authentication
+var app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// Routes
-app.use(routes);
-
-
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// // Requiring our routes
-app.use(routes)
+// Requiring our routes
+require("./routes/api/html-routes.js")(app);
+require("./routes/api/api-routes.js")(app);
 
-// // Syncing our database and logging a message to the user upon success
+// Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-module.exports = app;
-
